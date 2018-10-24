c8cf6dc07ecce08021f4f62a3928fffec3051aac
vijay.rana <vrana@sapient.com>
2018-10-24 13:42:42 +0100

    Server config for heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -38,6 +40,6 @@ app.get('/about', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('Sever is listning at 3000')
+app.listen(port, () => {
+  console.log(`Sever is listning at ${port}`);
 });
