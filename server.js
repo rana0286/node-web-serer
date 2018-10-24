92b0ae9f57dda3b386de915137431654f1784732
vijay.rana <vrana@sapient.com>
2018-10-24 14:29:27 +0100

    new route projects added

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.get('/about', (req, res) => {
   });
 })
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+})
+
 app.listen(port, () => {
   console.log(`Sever is listning at ${port}`);
 });
